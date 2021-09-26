b28701694e3513db2540e2418373b0b05abef660
ricardodsb <ricardojdasilva15@gmail.com>
2021-09-26 15:33:45 +0000

    cambios finales

--- src/app.js
+++ src/app.js
@@ -6,6 +6,31 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let words = excuseGenerate();
+  console.log(words);
+  document.querySelector("#excuse").innerHTML = words;
 };
+function excuseGenerate() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  return (
+    randomElement(who) +
+    " " +
+    randomElement(action) +
+    " " +
+    randomElement(what) +
+    " " +
+    randomElement(when)
+  );
+}
+function randomElement(Array) {
+  return Array[Math.floor(Math.random() * Array.length)];
+}
